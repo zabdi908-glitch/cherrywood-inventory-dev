e87ca7e2341a7a95f1b89ee6d2593fd8a7f64bdc
zabdi908-glitch <zabdi908@gmail.com>
2026-07-01 13:11:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
-from enquiries_store import enquiries_store
 from email_reply_agent import handle_enquiry_auto_reply
 import sqlite3
 import os
